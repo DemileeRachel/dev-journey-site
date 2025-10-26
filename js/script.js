cea2e883e6d310874b6115804e756855344b432a
DemileeRachel <demilee462@yahoo.com>
2025-10-26 15:40:37 +0000

    Button2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 console.log("Welcome to my Dev Journey site!");
 alert("Hello! Thanks for visiting my site 🚀");
+document.getElementById("greetBtn").addEventListener("click", function() {
+  alert("You clicked the button — welcome to my dev journey!");
+});
